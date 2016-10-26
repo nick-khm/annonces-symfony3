99a2b516789bb5ba3fddd071ace67dcecc5af2cd
Nick Kharaim <nick.kharaim@gmail.com>
2016-10-26 19:46:43 +0200

    Controllers

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -1,19 +1,54 @@
 <?php
-
 namespace OC\PlatformBundle\Controller;
 
-// N'oubliez pas ce use :
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class AdvertController extends Controller
 {
-  public function indexAction()
+  public function indexAction($page)
   {
-    $content = $this
-    ->get('templating')
-    ->render('OCPlatformBundle:Advert:index.html.twig', array('nom' => 'winzou'));
-    
-    return new Response($content);
+    if ($page < 1) {
+      throw new NotFoundHttpException('Page "'.$page.'" inexistante.');
+    }
+
+
+    return $this->render('OCPlatformBundle:Advert:index.html.twig');
+  }
+
+  public function viewAction($id)
+  {
+    return $this->render('OCPlatformBundle:Advert:view.html.twig', array(
+      'id' => $id
+    ));
+  }
+
+  public function addAction(Request $request)
+  {
+    if ($request->isMethod('POST')) {
+
+      $request->getSession()->getFlashBag()->add('notice', 'Annonce bien enregistrée.');
+
+      return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+    }
+
+    return $this->render('OCPlatformBundle:Advert:add.html.twig');
+  }
+
+  public function editAction($id, Request $request)
+  {
+    if ($request->isMethod('POST')) {
+      $request->getSession()->getFlashBag()->add('notice', 'Annonce bien modifiée.');
+
+      return $this->redirectToRoute('oc_platform_view', array('id' => 5));
+    }
+
+    return $this->render('OCPlatformBundle:Advert:edit.html.twig');
+  }
+
+  public function deleteAction($id)
+  {
+    return $this->render('OCPlatformBundle:Advert:delete.html.twig');
   }
 }
